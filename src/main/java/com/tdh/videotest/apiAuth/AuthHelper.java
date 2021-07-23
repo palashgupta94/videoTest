25c8391aa9adb79c46f8c3c2f0a9d4d1e040bbcd
palashgupta94 <palashgupta94@gmail.com>
2021-07-24 01:53:42 +0530

    5th commit with modified code

--- src/main/java/com/tdh/videotest/apiAuth/AuthHelper.java
+++ src/main/java/com/tdh/videotest/apiAuth/AuthHelper.java
@@ -1,5 +1,6 @@
 package com.tdh.videotest.apiAuth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
@@ -9,9 +10,7 @@ import org.apache.commons.codec.binary.Base64;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -50,21 +49,34 @@ public class AuthHelper {
         System.out.println("AAccess token =: " + responseEntity);
     }
 
-    public Map<String , String> getAccessCredentials(String authCode){
+    public static Map<String , String> updateAccessCredentialsMap(String authCode){
 
         Map<String , String>map = new LinkedHashMap<>();
 
         JsonObject object = new JsonObject(authCode);
 
-        map.put("access_token" , object.getString("access_token"));
-        map.put("expires_in" , object.getString("expires_in"));
-        map.put("refresh_token" , object.getString("refresh_token"));
-        map.put("scope" , object.getString("scope"));
-        map.put("token_type" , object.getString("token_type"));
+        if(object.containsKey("access_token")){
+            map.put("access_token" , object.getString("access_token"));
+        }
+
+        if(object.containsKey("expires_in")){
+            map.put("expires_in" , object.getString("expires_in"));
+        }
+
+        if(object.containsKey("refresh_token")){
+            map.put("refresh_token" , object.getString("refresh_token"));
+        }
+
+        if(object.containsKey("scope")){
+            map.put("scope" , object.getString("scope"));
+        }
+
+        if(object.containsKey("token_type")){
+            map.put("token_type" , object.getString("token_type"));
+        }
 
 
         System.out.println(map.toString());
-
         return map;
     }
 
@@ -74,20 +86,85 @@ public class AuthHelper {
 
     }
 
-    public static void writeToFile(String data , String filePath){
+    public static void updateFile(String data , String filePath){
 
         try {
+            JsonObject object = new JsonObject(data);
+            System.out.println("data = "+object);
+            JsonObject jso = getJsonObjectFromFile(filePath);
+            jso.put("access_token" , object.getString("access_token"));
+            jso.put("expires_in" , object.getString("expires_in"));
+            jso.put("scope" , object.getString("scope"));
+            jso.put("token_type" , object.getString("token_type"));
+            if(object.containsKey("refresh_token")){
+                jso.put("refresh_token" , object.getString("refresh_token"));
+            }
+            String jsonToWrite = Json.encodePrettily(jso);
             FileWriter file = new FileWriter(filePath);
 
-            for(int i = 0; i < data.length(); i++){
-                file.write(data.charAt(i));
+            for(int i = 0; i < jsonToWrite.length(); i++){
+                file.write(jsonToWrite.charAt(i));
             }
+            file.close();
+
+
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
 
+
     }
+
+    public static String getDataFromFile(String fileName){
+
+        String fileString="";
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            StringBuilder sb = new StringBuilder();
+            String str = "";
+            while((str=br.readLine()) != null){
+                sb.append(str +System.lineSeparator());
+            }
+            fileString = sb.toString();
+            return fileString;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("from file = "+fileString );
+        return fileString;
+
+    }
+    public static JsonObject getJsonObjectFromFile(String filePath){
+
+        String fileData = getDataFromFile(filePath);
+        JsonObject object = new JsonObject(fileData);
+        return object;
+
+    }
+
+    public static Map<String , String> getMapFromJsonObject(String filePath1 , String filePath2){
+
+        JsonObject obj1 = getJsonObjectFromFile(filePath1);
+        JsonObject obj2 = getJsonObjectFromFile(filePath2);
+
+        Map<String , String> credsMap = new LinkedHashMap<>();
+        credsMap.put("access_token" , obj1.getString("access_token"));
+        credsMap.put("expires_in" , obj1.getString("expires_in"));
+        credsMap.put("scope" , obj1.getString("scope"));
+        credsMap.put("token_type" , obj1.getString("token_type"));
+        credsMap.put("client_id" , obj2.getString("client_id"));
+        credsMap.put("client_secret" , obj2.getString("client_secret"));
+
+        return credsMap;
+
+    }
+
+
 
 }
--- src/main/java/com/tdh/videotest/controller/RestApiController.java
+++ src/main/java/com/tdh/videotest/controller/RestApiController.java
@@ -2,6 +2,7 @@ package com.tdh.videotest.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tdh.videotest.apiAuth.AuthHelper;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import org.springframework.http.HttpEntity;
@@ -12,7 +13,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import javax.security.auth.login.CredentialException;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -26,8 +31,9 @@ import java.util.Map;
 public class RestApiController {
 
     Map<String , String> map = null;
-    public static final String CREDENTIAL_FILE="C:\\Users\\HOME\\Downloads\\videotest\\videotest\\credentials.json";
-
+    public static final String CREDENTIAL_FILE="C:\\Users\\HOME\\Downloads\\videotest\\videotest\\src\\main\\resources\\static\\images\\credFiles\\credentials.json";
+    public static  final String API_KEY ="AIzaSyD_nsPtLhllJpazm9IcvhZtapdY0xHo9jw";
+    public static final String UNAUTHORIZED_STRING = "unauthorized";
     @GetMapping("/code")
     public String getCode(@RequestParam("code")String code){
         System.out.println("Code = "+ code);
@@ -70,11 +76,7 @@ public class RestApiController {
             }
             String responseString = sb.toString();
 
-            map = new AuthHelper().getAccessCredentials(responseString);
-
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.writeValue(new File(CREDENTIAL_FILE) , responseString);
-
+            AuthHelper.updateFile(responseString , CREDENTIAL_FILE);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -83,14 +85,9 @@ public class RestApiController {
     }
 
     @GetMapping("/refresh")
-    private String getNewAccessToken() {
+    private void getNewAccessToken() {
 
         String responseString="";
-
-//        String newParameters = "grant_type=refresh_token&";
-//        newParameters += "refresh_token="+map.get("refresh_token")+"&";
-//        newParameters += "client_id=154618135621-f1iuf522m78khr2r8q63nce69jrv6dk4.apps.googleusercontent.com&";
-//        newParameters += "client_secret=tzE7ldNiSlKSgXLX6kIZwV38";
         String newParameters = "grant_type=refresh_token&";
         newParameters += "refresh_token="+"1//0g_DHzaL_6VB-CgYIARAAGBASNwF-L9IrDjMxCr-b9AwHA8Ini4jfevU0RMDwDuO3MkQ7pjJBYBJjHiPtKJEbdTvEbqmGLANVnTQ"+"&";
         newParameters += "client_id=154618135621-f1iuf522m78khr2r8q63nce69jrv6dk4.apps.googleusercontent.com&";
@@ -121,71 +118,44 @@ public class RestApiController {
             }
             StringBuilder sb = new StringBuilder();
             BufferedReader in = new BufferedReader(new InputStreamReader(is));
-            String line = null;
+            String line;
             while((line = in.readLine()) != null) {
                 System.out.println(line);
                 sb.append(line);
             }
             responseString = sb.toString();
-            AuthHelper gac = new AuthHelper();
-            map = gac.getAccessCredentials(responseString);
+            AuthHelper.updateFile(responseString , CREDENTIAL_FILE);
+            map = AuthHelper.updateAccessCredentialsMap(responseString);
 
         }catch(Exception e){
             e.printStackTrace();
         }
-        return responseString;
-
     }
 
     @GetMapping("/getData")
     public Map<String , String> getYoutubeApiData(String accessToken) throws IOException {
-
-//        System.out.println("Global map = "+map.toString());
+       int retryMax = 5;
+       int retryCount = 0;
         String uploads="";
         String videoCount="";
-
-        Map<String , String> valueMap = new LinkedHashMap<>();
-
-        RestTemplate template = new RestTemplate();
-        String channelId ="UCoIXuO8Aco0M3-4MFSyZ_DA";
-        String apiKey = "AIzaSyD_nsPtLhllJpazm9IcvhZtapdY0xHo9jw";
-        String url = "https://www.googleapis.com/youtube/v3/channels";
-        String parts = "contentDetails,statistics";
-        String getUrl = url+"?part="+parts+"&id="+channelId+"&key="+apiKey;
-
-        HttpHeaders headers = new HttpHeaders();
-//        System.out.println("access token = "+map.get("access_token"));
-        headers.set("Authorization" , "Bearer "+accessToken);
-        headers.set("Accept" , "application/json");
-
-        HttpEntity request = new HttpEntity(headers);
-
-        ResponseEntity<String> response = template.exchange(getUrl , HttpMethod.GET , request , String.class);
-
-        if(response.getStatusCodeValue() >= 400){
-
-            HttpHeaders headers1 = response.getHeaders();
-            List<String> headerResponse = headers1.get("WWW-Authenticate");
-            if(headerResponse!=null && headerResponse.get(0).contains("invalid_token")){
-
-                String newAccessToken = getNewAccessToken();
-
-                JsonObject object = new JsonObject(newAccessToken);
-                ObjectMapper mapper = new ObjectMapper();
-                mapper.writeValue((new File()) , newAccessToken);
-                String value = mapper.readValue((new File(CREDENTIAL_FILE)), String.class);
-                JsonObject jso = new JsonObject(value);
-
-                newAccessToken = jso.getString("access_token");
-                getYoutubeApiData(newAccessToken);
-            }
-
-        }
-
-//        ObjectMapper mapper = new ObjectMapper();
         try {
-            JsonObject object = new JsonObject(response.getBody().toString());
+            String response = fireYoutubeDataApi(accessToken);
+            if (response!=null) {
+                if (response.equalsIgnoreCase(UNAUTHORIZED_STRING)) {
+                    while (retryCount++ < retryMax){
 
+                        getNewAccessToken();
+//                newAccessToken = AuthHelper.getDataFromFile(CREDENTIAL_FILE);
+                        JsonObject jso = AuthHelper.getJsonObjectFromFile(CREDENTIAL_FILE);
+                        String newAccessToken = jso.getString("access_token");
+                        response = fireYoutubeDataApi(newAccessToken);
+                        if (!response.equals(UNAUTHORIZED_STRING)) {
+                            break;
+                        }
+                    }
+                }
+            }
+            JsonObject object = new JsonObject(response);
             if(object != null){
                 JsonArray nodeArr = object.getJsonArray("items");
                 JsonObject item = null;
@@ -196,17 +166,60 @@ public class RestApiController {
                 uploads = item.getJsonObject("contentDetails").getJsonObject("relatedPlaylists").getString("uploads");
                 System.out.println("uploads = " + uploads);
 
-//                videoCount = item.getJsonObject("statistics").getString("videoCount");
                 System.out.println("videoCount = " + videoCount);
             }
+            return getYoutubePlaylistData(accessToken , API_KEY , uploads);
 
-//            JsonNode node = mapper.readTree(response.getBody());
-//            System.out.println("node = " + node);
-        } catch ( Exception e) {
-            e.printStackTrace();
+//            if(response.getStatusCodeValue() >= 400){
+//
+//            HttpHeaders headers1 = response.getHeaders();
+//            List<String> headerResponse = headers1.get("WWW-Authenticate");
+//            if(headerResponse!=null && headerResponse.get(0).contains("invalid_token")){
+//
+//                String newAccessToken = getNewAccessToken();
+//                AuthHelper.updateFile(newAccessToken , CREDENTIAL_FILE);
+//
+////                newAccessToken = AuthHelper.getDataFromFile(CREDENTIAL_FILE);
+//                JsonObject jso = AuthHelper.getJsonObjectFromFile(CREDENTIAL_FILE);
+//                newAccessToken = jso.getString("access_token");
+//                getYoutubeApiData(newAccessToken);
+//            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
         }
+        return null;
+    }
 
-        return getYoutubePlaylistData(accessToken , apiKey , uploads);
+    private String  fireYoutubeDataApi(String accessToken) throws  Exception{
+
+        try {
+            Map<String , String> valueMap = new LinkedHashMap<>();
+
+            RestTemplate template = new RestTemplate();
+            String channelId ="UCoIXuO8Aco0M3-4MFSyZ_DA";
+            String apiKey = "AIzaSyD_nsPtLhllJpazm9IcvhZtapdY0xHo9jw";
+            String url = "https://www.googleapis.com/youtube/v3/channels";
+            String parts = "contentDetails,statistics";
+            String getUrl = url+"?part="+parts+"&id="+channelId+"&key="+apiKey;
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization" , "Bearer "+accessToken);
+            headers.set("Accept" , "application/json");
+
+            HttpEntity request = new HttpEntity(headers);
+
+            ResponseEntity<String> response = template.exchange(getUrl , HttpMethod.GET , request , String.class);
+            System.out.println("response = "+response);
+            if (response!=null && response.getStatusCodeValue() == 200){
+                return response.getBody();
+            }
+        } catch (RestClientException e) {
+            e.printStackTrace();
+            if (e instanceof HttpClientErrorException.Unauthorized){
+                return UNAUTHORIZED_STRING;
+            }
+        }
+        return null;
     }
 
     private Map<String, String> getYoutubePlaylistData(String accessToken, String apiKey, String uploads) {
@@ -240,19 +253,19 @@ public class RestApiController {
     @GetMapping("/start")
     public void getStart(){
 
-        File jsonFile = new File("C:\\Users\\HOME\\Downloads\\videotest\\videotest\\credentials.json");
+        File jsonFile = new File( CREDENTIAL_FILE);
         if(jsonFile.exists()){
 
-            ObjectMapper mapper = new ObjectMapper();
+//            ObjectMapper mapper = new ObjectMapper();
             try {
-                String credString = mapper.readValue(new File("C:\\Users\\HOME\\Downloads\\videotest\\videotest\\credentials.json"), String.class);
+//                String credString = mapper.readValue(new File(CREDENTIAL_FILE), String.class);
+                String credString = AuthHelper.getDataFromFile(CREDENTIAL_FILE);
                 if(credString!= null && !credString.equalsIgnoreCase("")) {
 
                     JsonObject credObject = new JsonObject(credString);
                     String authToken = credObject.getString("access_token");
                     System.out.println("auth = " + authToken);
                     Map<String, String> youtubeApiData = getYoutubeApiData(authToken);
-//                    System.out.println("dataMap = " + youtubeApiData.toString());
                 }
                 else{
                     getAuthToken();
